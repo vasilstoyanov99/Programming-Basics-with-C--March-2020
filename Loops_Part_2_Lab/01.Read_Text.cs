91f6a125f1d21ffc9e6b60f0021a03eda05c187e
vasilstoyanov99 <63401488+vasilstoyanov99@users.noreply.github.com>
2020-04-11 17:32:54 +0300

    Create 01.Read_Text.cs

---
+++ Loops_Part_2_Lab/01.Read_Text.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _01.Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
