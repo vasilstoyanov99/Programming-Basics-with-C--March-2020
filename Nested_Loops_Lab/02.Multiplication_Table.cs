48285c692aad1c45dad2d2a4c6fcda88f4e46565
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-04-25 18:48:02 +0300

    Create 02.Multiplication_Table.cs

---
+++ Nested_Loops_Lab/02.Multiplication_Table.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _02.Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.WriteLine($"{x} * {y} = {x * y}");
+                }
+            }
+        }
+    }
+}
